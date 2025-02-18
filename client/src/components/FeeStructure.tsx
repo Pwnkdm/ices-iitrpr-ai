2d6f461109918844f0cd2b22cba99ddafa853a64
pwnkdm <pawanrajkadam@gmail.com>
2025-02-18 17:34:07 +0530

    removed the line in fees table

--- client/src/components/FeeStructure.tsx
+++ client/src/components/FeeStructure.tsx
@@ -1,40 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./feeStructure.css";
 
 export const FeeStructure: React.FC = () => {
   return (
-    <div className='feeScomponent_Parent'>
-          <div className='feeHeading'>
-              <span>Fee</span> <span> Structure</span>
+    <div className="feeScomponent_Parent">
+      <div className="feeHeading">
+        <span>Fee</span> <span> Structure</span>
+      </div>
+      <br></br>
+      <div className="feesTableHeadingRows">
+        <div className="rows-eachFees">
+          <div className="columnOne">
+            <div>Registration Fees</div>
+            <span>(Non-Refundable)</span>
           </div>
-          <br></br>
-            <div className='feesTableHeadingRows'>
-                    <div className='rows-eachFees'>
-                      <div className="columnOne">
-                          <div>Registration Fees</div>
-                          <span>(Non-Refundable)</span>
-                      </div>
-                      <div className="columnTwo">INR 500</div>
-                    </div>
+          <div className="columnTwo">INR 500</div>
+        </div>
 
-                    <div className="rows-eachFees">
-                      <div className="columnOne">
-                          <div>Program Fees</div>
-                          <span>(Non-Refundable)</span>
-                      </div>
-                      <div className="columnTwo">INR 60,000</div>
-                    </div>
-                    <div className='rows-eachFees'>
-                      <div className="columnOne">Total</div>
-                      <div className="columnTwo">INR 60,500</div>
-                    </div>
-                    <div className='rows-eachFees feetermparent'>
-                      <div className="feesTerms"><span>* Eligible for Skill Loan</span></div>
-                    </div>
-                  
-            </div>
-          
-
+        <div className="rows-eachFees">
+          <div className="columnOne">
+            <div>Program Fees</div>
+            <span>(Non-Refundable)</span>
+          </div>
+          <div className="columnTwo">INR 60,000</div>
+        </div>
+        <div className="rows-eachFees">
+          <div className="columnOne">Total</div>
+          <div className="columnTwo">INR 60,500</div>
+        </div>
+        <div className="rows-eachFees feetermparent"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
