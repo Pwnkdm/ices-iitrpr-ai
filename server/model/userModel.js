604a2269921d8c60d32f972fc818977b3413b539
Prashant-KT <tiwariprashant0395@gmail.com>
2025-02-19 16:43:33 +0530

    country code

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {type: String,required:true},
     email: {type: String,required:true,unique:true},
+    // countryCode: {type: String,required:true},
     phonenumber: {type: String,required:true,unique:true},
 },{
     timestamps: true,
