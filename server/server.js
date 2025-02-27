8313b2c8b23e5a7ed4cd5a3ba1c0acf7b999493f
pwnkdm <pawanrajkadam@gmail.com>
2025-02-27 14:56:06 +0530

    added one more option

--- server/server.js
+++ server/server.js
@@ -1,32 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/dbConfig.js';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/dbConfig.js";
+import cors from "cors";
 const app = express();
 import userRouter from "./routes/userRoute.js";
-import totRouter from "./routes/totRoute.js"
+import totRouter from "./routes/totRoute.js";
 
-dotenv.config({path: './config.env'});
-app.use(cors({
-    origin: process.env.BASE_URL, 
-    methods: "GET,POST", 
+dotenv.config({ path: "./config.env" });
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    methods: "GET,POST,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
-  }));
-
-
+  })
+);
 
 app.use(express.json());
 
 connectDB();
 
-app.use('/user',userRouter);
-app.use('/tot',totRouter)
+app.use("/user", userRouter);
+app.use("/tot", totRouter);
 
 const PORT = process.env.PORT || 5000;
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
