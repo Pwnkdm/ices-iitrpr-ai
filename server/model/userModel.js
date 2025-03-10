cb45f212c3b2d975f7b9b20534df846ca8001317
Prashant-KT <tiwariprashant0395@gmail.com>
2025-03-10 15:27:13 +0530

    msg

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
     email: {type: String,required:true,unique:true},
     // countryCode: {type: String,required:true},
     phonenumber: {type: String,required:true,unique:true},
+    collegeName: {type: String,required:true},
+    collegeAddress: {type: String,required:true},
+    city: {type: String,required:true},
+    pincode: {type: String,required:true},
 },{
     timestamps: true,
     versionKey: false 
